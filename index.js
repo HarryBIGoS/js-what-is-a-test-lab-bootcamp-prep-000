6960292b7594ef86bc28bad444d123c8ddd8622e
Harry Bigos <hrbigos@gmail.com>
2018-08-13 19:55:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ var message = `${name} is ${height} inches tall`
 module.exports = {name, height, message}
 
 
-if () {
-  
+if (name === "Joe") {
+  name = "Susan";
 }
+else {
+  console.log("You're not good at this at all are you, Harry?")
+}
+
+
+if (height > 40) {
+  var height = 40;
+}
+else {
+  console.log("COME ON, BRUV")
+}
+
